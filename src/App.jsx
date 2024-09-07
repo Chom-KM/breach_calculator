0bca13946aecaea777ac5d0a5c1e14094fdff02a
KM-Chom <kasamapornchom@gmail.com>
2024-09-07 19:37:54 +0700

    add google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import Calculator from "./Calculator";
 import { questions } from "./Questions";
+import ReactGA from 'react-ga4';
+
+ReactGA.initialize('G-N16Q3ME9BZ');
 
 function App() {
   return (
